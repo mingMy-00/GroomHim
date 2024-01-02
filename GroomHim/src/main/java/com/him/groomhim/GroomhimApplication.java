dc1e08aa3cb1b2642979d09fe5246d127032c0c3
Afdddd <kimdlsduq36@gmail.com>
2024-01-02 14:47:43 +0900

    [ADD] 프로젝트 초기 세팅 - 인엽
    - java 17
    - gradle 8.5
    - springboot 3.2.1
    - package jar
    
    Dependencies
    - Spring Web
    - Spring Data JPA
    - MySQL Driver
    - Lombok

---
+++ GroomHim/src/main/java/com/him/groomhim/GroomhimApplication.java
@@ -0,0 +1,13 @@
+package com.him.groomhim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroomhimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroomhimApplication.class, args);
+	}
+
+}
---
+++ GroomHim/src/test/java/com/him/groomhim/GroomhimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.him.groomhim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroomhimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
