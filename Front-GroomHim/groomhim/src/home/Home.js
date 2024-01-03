7bd9215e3a84845efa95548e5ea022b3767eeaa7
김인엽 <109032883+Afdddd@users.noreply.github.com>
2024-01-04 02:30:22 +0900

    [STYLE] Home - 인엽

--- Front-GroomHim/groomhim/src/home/Home.js
+++ Front-GroomHim/groomhim/src/home/Home.js
@@ -1,14 +1,18 @@
-import logo from "./assets/imgs/logo.png";
+import "./Home.css";
+import logo from "../assets/imgs/logo.png";
+import {Link} from 'react-router-dom';
 
 function Home(){
     return(
-        <div>
-            <img src={logo}/>
-            
-            <p>질문의 답을 하고 나의 피부타입에 맞는 화장품을 추천받자</p>
-
-
-        </div>
+        <>
+            <div className="container">
+                <img className="logo" src={logo}/>
+                <h2>질문의 답을 하고 나의 피부타입에 맞는 화장품을 추천받자</h2>
+            <Link to="/skinTest" className="test_btn">
+                추천받기
+            </Link>
+            </div>
+        </>
     );
 }
 
