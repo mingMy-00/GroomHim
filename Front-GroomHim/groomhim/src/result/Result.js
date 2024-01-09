aba4ab2b2dcc2c9a225dac0c7d0e7fde6b09fad4
mingMy-00 <149574203+mingMy-00@users.noreply.github.com>
2024-01-10 03:47:28 +0900

    [Add] 결과 설문지 -김민제

--- Front-GroomHim/groomhim/src/result/Result.js
+++ Front-GroomHim/groomhim/src/result/Result.js
@@ -14,6 +14,24 @@ function Result(){
         <div>
             <div className='result-container'>
                 <h1> {memberName}님의 피부타입은 <b class="b">{skinType}</b> 입니다.</h1>
+                <div id="margin">
+                <div class="tooltip-container">
+                    <span class="text">자세한 정보👀</span>
+                    <span className="introDetail">           
+                        Q. 건성피부란  ? <br />
+                        A. 피부의 유분(기름)과 수분(촉촉함)이 부족해서 <br />
+                           표면이 거칠고 얼굴이 땅기는 피부타입이에요. <br /><br />
+
+                            유분제품을 너무 많이 바르면 모공을 막아서
+                            좁쌀여드름이 나고
+                              <br />
+                            수분제품은 보통 가벼운 액체 타입이라서 날아가기 쉬워요.
+                            <br /><br />
+                        
+                        ✔ 따라서, 보습제품과 유수분 밸런스가 중요합니다.</span>
+                </div>
+                </div>
+
                 <div id="SkinIntro">
                     <div id="intro">
                     이 타입은 <b className="b">보습제품</b>과 <b className="b">유수분 밸런스</b>가 중요합니다.
@@ -35,19 +53,32 @@ function Result(){
                     </table>
                 </div>
                 </div>
-                <div className="buttonList">
-                    <button>토너 <br />Top5</button>
-                    <button>수분제품 Top5</button>
-                    <button>보습크림 Top5</button>
-                </div>
+                <div class="radio-inputs">
+                    <label class="radio">
+                        <input type="radio" name="radio" checked=""/>
+                        <span class="name">토너 Top5</span>
+                    </label>
+                    <label class="radio">
+                        <input type="radio" name="radio"/>
+                        <span class="name">수분제품 Top5</span>
+                    </label>
+                        
+                    <label class="radio">
+                        <input type="radio" name="radio"/>
+                        <span class="name">보습크림 Top5</span>
+                    </label>
+                    </div>
                                
                 <div className='product-list'>
                     <div class="product-container">
                         <div class="product">
-                            <img src={logo} alt="Product Image 1" />
+                            1.
+                            <img src="https://image8.coupangcdn.com/image/retail/images/284614092420512-ace274de-9ed5-4b97-82c1-343be2dae1c8.jpg" alt="Product Image 1" />
                             <div class="product-info">
-                                <h2>Product Name 1</h2>
-                                <p>$19.99</p>
+                                <table className="productTable">
+                                    <tr><td><h5>이니스프리 New 그린티 씨드 히알루론산 고수분 크림 50ml</h5></td></tr>
+                                    <tr><td><h6>16,650원</h6></td></tr>
+                                </table>
                             </div>
                             <button class="purchase-button">Buy</button>
                             <button class="scrap-button">Scrap</button>
@@ -55,10 +86,13 @@ function Result(){
                     </div>
                     <div class="product-container">
                         <div class="product">
-                            <img src={logo} alt="Product Image 1" />
+                            2.
+                            <img src="https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/284609073615780-32647246-1d35-4b96-8da4-262a39f7a279.jpg" alt="Product Image 1" />
                             <div class="product-info">
-                                <h2>Product Name 2</h2>
-                                <p>$19.99</p>
+                                <table className="productTable">
+                                    <tr><td><h5>닥터지 레드 블레미쉬 시카 수딩 크림 듀오 기획세트 50ml</h5></td></tr>
+                                    <tr><td><h6>32,770원</h6></td></tr>
+                                </table>
                             </div>
                             <button class="purchase-button">Buy</button>
                             <button class="scrap-button">Scrap</button>
@@ -66,10 +100,13 @@ function Result(){
                     </div>
                     <div class="product-container">
                         <div class="product">
-                            <img src={logo} alt="Product Image 1" />
+                            3.
+                            <img src="https://thumbnail10.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/9508847276305564-ae3485c2-a561-4df9-87aa-607e4e49b0ae.jpg" />
                             <div class="product-info">
-                                <h2>Product Name 3</h2>
-                                <p>$19.99</p>
+                                <table className="productTable">
+                                    <tr><td><h5>일리윤 히알루론 모이스춰 수분크림 100ml, 1개</h5></td></tr>
+                                    <tr><td><h6>7,670원</h6></td></tr>
+                                </table>
                             </div>
                             <button class="purchase-button">Buy</button>
                             <button class="scrap-button">Scrap</button>
@@ -77,10 +114,13 @@ function Result(){
                     </div>
                     <div class="product-container">
                         <div class="product">
-                            <img src={logo} alt="Product Image 1" />
+                            2.
+                            <img src="https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/284609073615780-32647246-1d35-4b96-8da4-262a39f7a279.jpg" alt="Product Image 1" />
                             <div class="product-info">
-                                <h2>Product Name 4</h2>
-                                <p>$19.99</p>
+                                <table className="productTable">
+                                    <tr><td><h5>닥터지 레드 블레미쉬 시카 수딩 크림 듀오 기획세트 50ml</h5></td></tr>
+                                    <tr><td><h6>32,770원</h6></td></tr>
+                                </table>
                             </div>
                             <button class="purchase-button">Buy</button>
                             <button class="scrap-button">Scrap</button>
@@ -88,10 +128,13 @@ function Result(){
                     </div>
                     <div class="product-container">
                         <div class="product">
-                            <img src={logo} alt="Product Image 1" />
+                            2.
+                            <img src="https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/284609073615780-32647246-1d35-4b96-8da4-262a39f7a279.jpg" alt="Product Image 1" />
                             <div class="product-info">
-                                <h2>Product Name 4</h2>
-                                <p>$19.99</p>
+                                <table className="productTable">
+                                    <tr><td><h5>닥터지 레드 블레미쉬 시카 수딩 크림 듀오 기획세트 50ml</h5></td></tr>
+                                    <tr><td><h6>32,770원</h6></td></tr>
+                                </table>
                             </div>
                             <button class="purchase-button">Buy</button>
                             <button class="scrap-button">Scrap</button>
