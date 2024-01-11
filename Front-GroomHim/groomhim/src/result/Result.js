600149c9b424a2b1fd1b0da14fde3d881eb4ff53
mingMy-00 <149574203+mingMy-00@users.noreply.github.com>
2024-01-11 22:58:48 +0900

    [Add] 유튜브링크 -김민제

--- Front-GroomHim/groomhim/src/result/Result.js
+++ Front-GroomHim/groomhim/src/result/Result.js
@@ -33,7 +33,8 @@ function Result(){
                             <td>수분을 눌러서 유지해줄 보습크림.</td>
                         </tr>
                     </table>
-                </div>
+                    <iframe width="350" height="315" src="https://www.youtube.com/embed/NnwA8DTTpvg?si=MLITPcsQ4mZ8-vL4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
                 </div>
                 
                 <div class="radio-inputs">
