ac48c6647a0f24982b228a97a7a080069a235954
mingMy-00 <149574203+mingMy-00@users.noreply.github.com>
2024-01-12 18:04:11 +0900

    [Add] 회원 정보 업뎃 + 화장품 정보 불러오기 -김민제

--- Front-GroomHim/groomhim/src/result/Result.js
+++ Front-GroomHim/groomhim/src/result/Result.js
@@ -1,50 +1,170 @@
 import './Result.css';
 import logo from "../assets/imgs/logo.png";
 import { useLocation } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 
 function Result(){
-
+    /* SkinTest.js로 부터 전달받은 값 */
     const location = useLocation();
-    const skinType = location.state.skinType[0].skinType;
-  
+    /* 피부타입을 위해서 만든 useState */
+    const [skinType, setSkinType] = useState(location.state.skinType[0].skinType);
+    /*로그인한 회원 정보*/   
     const storedData    = JSON.parse(sessionStorage.getItem("loginMember"));
     const memberName    = storedData ? storedData.memberName : null;
-    //라이도 버튼의 상태를 관리
+    const memberNo      = storedData ? storedData.memberNo   : null;
+    //라디오 버튼의 상태를 관리
     const [selectedRadio, setSelectedRadio] = useState('토너');
 
     //라디오 체크하면 실행되는 함수
     const handleRadioClick = (name) => {
+        //name은 화장품의 종류임. (ex: 크림, 세럼, 토너)
         setSelectedRadio(name);
+        console.log(name);
+        //회원 정보에 피부타입 업데이트를 위한 axios
+        axios({
+            url : "/result",
+            method : "post",
+            data : {memberNo : memberNo, skinType : skinType}
+        }).then(function(response1) {
+                console.log("회원 정보 업데이트 성공" + response1);
+                console.log("axios 두번째 실행하러감");
+                //화장품 정보 불러오기 위한 axios
+                axios({
+                    url : "/result/recommend",
+                    method : "post",
+                    data : {memberNo : memberNo, beautyType : name}
+                }).then(function(response2) {
+                    console.log("화장품 정보 불러오기 성공 : " + response2);
+                }).catch(function() {
+                    console.log("화장품 정보 불러오기 실패");
+                });
+        }).catch(function() {
+            console.log("회원 정보 insert 실패 ㅋ");
+        });
     };
+
+    /*SkinType 설정하는 것*/
+    useEffect(() => {
+        setSkinType(skinType);
+    }, []);
+        /*피부타입에 따라 넣어줄 설명*/ 
+        const renderSkinIntro = () => {
+            if (skinType === '건성') {
+              return (
+                <div id="SkinIntro">
+                  이 타입은 <b className="b">보습제품</b>과 <b className="b">유수분 밸런스</b>가 중요합니다.
+                  <br></br>
+                  <br />
+                  <table id="table">
+                    <tr>
+                      <td>Step 1. </td>
+                      <td>수분을 흡수시켜줄 토너</td>
+                    </tr>
+                    <tr>
+                      <td>Step 2.</td>
+                      <td>수분 가득 제품</td>
+                    </tr>
+                    <tr>
+                      <td>Step 3.</td>
+                      <td>수분을 눌러서 유지해줄 보습크림.</td>
+                    </tr>
+                  </table>
+                  <iframe
+                    width="350"
+                    height="315"
+                    src="https://www.youtube.com/embed/NnwA8DTTpvg?si=MLITPcsQ4mZ8-vL4"
+                    title="YouTube video player"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen
+                  ></iframe>
+                </div>
+              );
+            } else if(skinType === '수부지') {
+                return (
+                    <div id="SkinIntro">
+                        "이건 수부지 설명"
+                    </div>
+                );
+            } else {
+                    <div id="SkinIntro">
+                        "이건 지성 설명"
+                    </div>
+            } 
+          };
+
+    /*피부타입에 따라 넣어줄 자세한 정보*/ 
+    const introDetail = () => {
+        if(skinType === '건성') {
+            return(
+            <div id="margin">
+                        <div class="tooltip-container">
+                            <span class="text">자세한 정보👀</span>
+                            <span className="introDetail">           
+                                Q. 건성피부란  ? <br />
+                                A. 피부의 유분(기름)과 수분(촉촉함)이 부족해서 <br />
+                                표면이 거칠고 얼굴이 땅기는 피부타입이에요. <br /><br />
+
+                                    유분제품을 너무 많이 바르면 모공을 막아서
+                                    좁쌀여드름이 나고
+                                    <br />
+                                    수분제품은 보통 가벼운 액체 타입이라서 날아가기 쉬워요.
+                                    <br /><br />
+                                
+                                ✔ 따라서, 보습제품과 유수분 밸런스가 중요합니다.
+                            </span>
+                        </div>
+            </div>
+            );
+        }else if(skinType === '수부지') {
+            return(
+                <div id="margin">
+                            <div class="tooltip-container">
+                                <span class="text">자세한 정보👀</span>
+                                <span className="introDetail">           
+                                    Q. 수부지피부란  ? <br />
+                                    A. 피부의 유분(기름)과 수분(촉촉함)이 부족해서 <br />
+                                    표면이 거칠고 얼굴이 땅기는 피부타입이에요. <br /><br />
+    
+                                        유분제품을 너무 많이 바르면 모공을 막아서
+                                        좁쌀여드름이 나고
+                                        <br />
+                                        수분제품은 보통 가벼운 액체 타입이라서 날아가기 쉬워요.
+                                        <br /><br />
+                                    
+                                    ✔ 따라서, 보습제품과 유수분 밸런스가 중요합니다.
+                                </span>
+                            </div>
+                </div>
+                );
+        }else {
+            return(
+                <div id="margin">
+                            <div class="tooltip-container">
+                                <span class="text">자세한 정보👀</span>
+                                <span className="introDetail">           
+                                    Q. 지성피부란  ? <br />
+                                    A. 피부의 유분(기름)과 수분(촉촉함)이 부족해서 <br />
+                                    표면이 거칠고 얼굴이 땅기는 피부타입이에요. <br /><br />
+    
+                                        유분제품을 너무 많이 바르면 모공을 막아서
+                                        좁쌀여드름이 나고
+                                        <br />
+                                        수분제품은 보통 가벼운 액체 타입이라서 날아가기 쉬워요.
+                                        <br /><br />
+                                    
+                                    ✔ 따라서, 보습제품과 유수분 밸런스가 중요합니다.
+                                </span>
+                            </div>
+                </div>
+                );
+        }};
 
     return(
         <div>
             <div className='result-container'>
                 <h1> {memberName}님의 피부타입은 <b class="b">{skinType}</b> 입니다.</h1>
-                <div id="SkinIntro">
-                    <div id="intro">
-                    이 타입은 <b className="b">보습제품</b>과 <b className="b">유수분 밸런스</b>가 중요합니다.
-                    <br></br>
-                    <br />
-                    <table id="table"> 
-                        <tr>
-                            <td>Step 1. </td>
-                            <td>수분을 흡수시켜줄 토너</td>
-                        </tr>
-                        <tr>
-                            <td>Step 2.</td>
-                            <td>수분 가득 제품</td>
-                        </tr>
-                        <tr>
-                            <td>Step 3.</td>
-                            <td>수분을 눌러서 유지해줄 보습크림.</td>
-                        </tr>
-                    </table>
-                    <iframe width="350" height="315" src="https://www.youtube.com/embed/NnwA8DTTpvg?si=MLITPcsQ4mZ8-vL4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                    </div>
-                </div>
-                
+                   {renderSkinIntro()}
                 <div class="radio-inputs">
                     <label class="radio">
                         <input type="radio" name="토너" className="radio" checked={selectedRadio === '토너'} onChange={() => handleRadioClick('토너')}/>
@@ -60,23 +180,7 @@ function Result(){
                         <span class="name">보습크림 Top5</span>
                     </label>
                 </div>
-                        <div id="margin">
-                        <div class="tooltip-container">
-                            <span class="text">자세한 정보👀</span>
-                            <span className="introDetail">           
-                                Q. 건성피부란  ? <br />
-                                A. 피부의 유분(기름)과 수분(촉촉함)이 부족해서 <br />
-                                표면이 거칠고 얼굴이 땅기는 피부타입이에요. <br /><br />
-
-                                    유분제품을 너무 많이 바르면 모공을 막아서
-                                    좁쌀여드름이 나고
-                                    <br />
-                                    수분제품은 보통 가벼운 액체 타입이라서 날아가기 쉬워요.
-                                    <br /><br />
-                                
-                                ✔ 따라서, 보습제품과 유수분 밸런스가 중요합니다.</span>
-                        </div>
-                </div>
+                {introDetail()}
 
                 <div className='product-list'>
                     <div class="product-container">
