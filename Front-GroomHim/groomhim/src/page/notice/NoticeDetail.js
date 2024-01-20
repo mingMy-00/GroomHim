2b7ff16d8f9841564a2c07d29505314bc4a472f2
mingMy-00 <149574203+mingMy-00@users.noreply.github.com>
2024-01-21 00:19:59 +0900

    [Add] 공지사항 삭제 - 김민제

--- Front-GroomHim/groomhim/src/page/notice/NoticeDetail.js
+++ Front-GroomHim/groomhim/src/page/notice/NoticeDetail.js
@@ -1,5 +1,4 @@
 import React, { useState , useEffect } from 'react';
-import Modal from 'react-modal';
 import { useLocation , useNavigate } from "react-router-dom";
 import './noticeDetail.css';
 import axios from 'axios';
@@ -10,7 +9,6 @@ const NoticeDetail = () => {
   let location = useLocation();
   let noticeNo = location.state.noticeNo;
   let navigate = useNavigate();
-  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   const out = () => {
     navigate("/page/notice/notice");
@@ -22,21 +20,21 @@ const NoticeDetail = () => {
 
   const deleteNotice = () => {
     axios({
-      url: `http://localhost:9090/noticeDelete?noticeNo=${noticeNo}`,
-      method: 'get'
+      url : "http://localhost:9090/noticeDelete?noticeNo=" + noticeNo ,
+      method : "get"
     })
-      .then(function (result) {
-        if (result.data === 1) {
-          setModalIsOpen(true);
-        } else {
-          alert('게시글 삭제에 실패했습니다. 관리자에게 문의해주세요.');
+    .then(function(result) {
+        if(result.data == 1) {
+          alert("게시글이 성공적으로 삭제 되었습니다.");
+          navigate("/page/notice/notice");
+        }else{
+          alert("게시글 삭제에 실패했습니다. 관리자에게 문의해주세요.");
         }
-      })
-      .catch(function () {
-        console.log('공지사항 게시글 삭제 실패');
-      });
+    })
+    .catch(function() {
+        console.log("공지사항 게시글 삭제 실패");
+    });
   };
-
 
 
   useEffect(function() {
@@ -65,17 +63,6 @@ const NoticeDetail = () => {
 
       <div className="notice-content">{notice.noticeContent}</div>
     <div className="updateButton"><button onClick={updateNotice}>수정하기</button><button onClick={deleteNotice}>삭제하기</button></div>
-    <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
-        <div>
-          <p>공지사항을 삭제하면 복구가 불가합니다. 정말로 삭제하시겠습니까?</p>
-          <button onClick={() => setModalIsOpen(false)}>취소</button>
-          <button onClick={() => {
-            setModalIsOpen(false);
-            alert('게시글이 성공적으로 삭제 되었습니다.');
-            
-          }}>확인</button>
-        </div>
-    </Modal>
     </div>
   );
 };
